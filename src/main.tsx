173b890fb1860a76a3dfb9e79c5db245ac577f42
rishiketh prabhakar <rishiketh1998@gmail.com>
2023-06-05 00:50:50 +0100

    add dashboard component

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={new QueryClient()}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -1,3 +1,53 @@
+import { useState } from "react";
+import { LinearProgress, Alert, Pagination } from "@mui/material";
+import { Dashboard } from "../../components/dashboard/dashboard";
+import { useQuery } from "react-query";
+
 export const Users = () => {
-  return <div>Users</div>;
+  const [page, setPage] = useState<number>(1);
+  const { isLoading, error, data } = useQuery(
+    ["users", { page }],
+    () => fetchUsersData(page),
+    {
+      retry: false,
+    }
+  );
+
+  const fetchUsersData = async (pageNo: number) => {
+    const response = await fetch(
+      `https://api.engageesp.com/ext/v1/users?page=${pageNo}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          tenantId: "66b7391b-7cf1-11ec-8440-0a8688f5a186",
+          Authorization: `Bearer FxDUomKYF2EKD7Mwc2eJ8Uu0nFkl11m8mpDnFggf`,
+        },
+      }
+    );
+
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(`Failed to fetch (${response.status}): ${data.error}`);
+    }
+    return data;
+  };
+
+  if (isLoading) return <LinearProgress />;
+
+  if (error instanceof Error)
+    return <Alert severity="error">{error.message}</Alert>;
+
+  return (
+    <>
+      <Dashboard data={data?.data} />
+      <Pagination
+        color="primary"
+        sx={{ mt: "20px", display: "flex", justifyContent: "center" }}
+        count={data.meta.last_page}
+        page={page}
+        onChange={(e, page) => setPage(page)}
+      />
+    </>
+  );
 };
